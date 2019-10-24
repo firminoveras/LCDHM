a2a2f2f3461f09c3f51b0bd03b6465d5fc302096
Firmino Veras <firmino.veras@live.com>
2019-10-24 20:56:42 -0300

    Remover Compilações do ESP32

--- LCDHM_ESP32/Release/VM_DBG/VM_mem_check_sam.cpp.d
+++
@@ -1,2 +0,0 @@
-C:\Users\firmi\AppData\Local\Temp\VMBuilds\LCDHM_ESP32\Heltec-esp32_wifi_lora_32_V2\Release\VM_DBG\VM_mem_check_sam.cpp.o: \
- c:\program\ files\ (x86)\microsoft\ visual\ studio\2019\community\common7\ide\extensions\cpwzuv43.p51\Micro\ Platforms\default\debuggers\VM_DBG\VM_mem_check_sam.cpp
